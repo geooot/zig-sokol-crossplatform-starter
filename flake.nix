a9bf610b56f68ddd0f8ba34fdad64c846a9097b4
George Thayamkery <george.thayamkery@gmail.com>
2025-05-10 21:44:33 -0700

    Add flake

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "An application";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    zig.url = "github:mitchellh/zig-overlay";
+
+    # Used for shell.nix
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  } @ inputs: let
+    overlays = [
+      # Other overlays
+      (final: prev: {
+        zigpkgs = inputs.zig.packages.${prev.system};
+      })
+    ];
+
+    # Our supported systems are the same supported systems as the Zig binaries
+    systems = builtins.attrNames inputs.zig.packages;
+  in
+    flake-utils.lib.eachSystem systems (
+      system: let
+        pkgs = import nixpkgs {inherit overlays system;};
+      in rec {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            zigpkgs."0.13.0"
+          ];
+
+          packages = with pkgs; [
+            neovim
+
+            # Packages required for building linux binary 
+            alsa-lib
+            libGL
+            xorg.libXi
+            xorg.libXcursor
+            xorg.libX11
+          ];
+        };
+
+        # For compatibility with older versions of the `nix` binary
+        devShell = self.devShells.${system}.default;
+      }
+    );
+}
